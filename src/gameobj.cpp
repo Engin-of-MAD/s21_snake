5687f9afa200d9629ac1447503772575c85aefcd
Ваше Имя <you@example.com>
2024-08-20 22:48:36 +0300

    update model

--- src/gameobj.cpp
+++ src/gameobj.cpp
@@ -1,50 +1,61 @@
 #include "gameobj.h"
 
+GameBoard::GameBoard() : GameBoard(10, 20){}
+GameBoard::GameBoard(int width, int height)
+{
+    m_gameField = new int*[height];
 
-Board::Board(int width, int height) : width(width), height(height) {
-    board = new int* [height];
-    for (size_t i = 0; i < height; ++i) {
-        board[i] = new int [width];
-    }
-    initBoard();
-}
-
-Board::~Board() {
-    if (board != nullptr) {
-        for (int i = 0; i < height; ++i) {
-            delete[] board;
-        }
-        delete board;
-        board = nullptr;
+    for (int i = 0; i < height; ++i) {
+        m_gameField[i] = new int[width];
     }
 }
 
-Shapes::~Shapes() {
-    if (array != nullptr) {
-        for (size_t i = 0; i < width; i++) {
-            delete[] array[i];
-        }
-    }
-    delete[] array;
-}
-
-
-
-//Shapes::Shapes(Shapes &other) : Shape(other) {}
-
-void Board::initBoard(){
-    for (size_t i = 0; i < height; ++i) {
-        for (size_t j = 0; j < width; ++j) {
-            board[i][j] = 0;
+GameBoard::GameBoard(GameBoard &other): GameBoard(other.m_width, other.m_height)
+{
+    for (int i = 0; i < other.m_height; ++i) {
+        for (int j = 0; j < other.m_width; ++j) {
+            m_gameField[i][j] = other.m_gameField[i][j];
         }
     }
 }
-GameObject::GameObject() : score(0)
-, timer(1000000)
-, state(START)
-, Table(new Board(10, 20))
-, current(new Shapes())
-, next(new Shapes)
-{}
+
+GameBoard::~GameBoard() {
+    if (m_gameField) {
+        for (int i = 0; i < m_height; ++i) {
+            delete[] m_gameField[i];
+        }
+        delete[] m_gameField;
+        m_gameField = nullptr;
+    }
+}
+
+int GameBoard::width() const { return m_width; }
+int GameBoard::height() const { return m_height; }
 
 
+
+Shape::Shape(int width, char name) : m_width(width)
+    , m_name(name)
+    , cordX(0)
+    , cordY(0)
+{
+    m_array = new int*[m_width];
+    for (int i = 0; i < m_width; ++i) {
+        m_array[i] = new int[m_width];
+    }
+}
+
+Shape::Shape(Shape& other) {
+
+}
+
+Shape::~Shape()
+{
+    if (m_array) {
+        for (int i = 0; i < m_width; ++i) {
+            delete[] m_array[i];
+        }
+        delete[] m_array;
+        m_array = nullptr;
+    }
+}
--- src/gameobj.h
+++ src/gameobj.h
@@ -4,77 +4,71 @@
 
 #ifndef BRICKGAME_GAMEOBJ_H
 #define BRICKGAME_GAMEOBJ_H
-#include "../brick_game/tetris/tetris.h"
-#include <initializer_list>
+// #include "../brick_game/tetris/tetris.h"
+// #include <initializer_list>
 
 
-class Board {
-    int** board = nullptr;
-    int width, height;
-    void initBoard();
+typedef enum {START = 0, SPAWN, MOVING, PAUSE, GAMEOVER, EXIT_STATE} stateApp;
+
+class GameBoard {
+    int** m_gameField;
+    int m_width, m_height;
 public:
-    explicit Board(int width, int height);
-    ~Board();
-
+    GameBoard();
+    explicit GameBoard(int width = 1, int height = 1);
+    GameBoard(GameBoard& other);
+    ~GameBoard();
+    int width() const;
+    int height() const;
 };
 
-
-struct Shapes : Shape {
-//    char **array; // shape
-//    char name;
-//    int row, col, width;;
-    Shapes() = default;
-
-//    Shapes(Shapes& other);
-    ~Shapes();
+struct Shape {
+    int **m_array; // shape
+    int m_width;
+    char m_name;
+    int cordX, cordY; // params shape
+    Shape();
+    Shape(int width, char name);
+    Shape(Shape& other);
+    ~Shape();
 };
-//Shapes S = {(char *[]){(char[]){0, 1, 1}, (char[]){1, 1, 0}, (char[]){0, 0, 0}}, 3, 'S', 0, 0};
-//Shapes Z = {(char *[]){(char[]){1, 1, 0}, (char[]){0, 1, 1}, (char[]){0, 0, 0}}, 3, 'Z', 0, 0};
-//Shapes T = {(char *[]){(char[]){0, 1, 0}, (char[]){1, 1, 1}, (char[]){0, 0, 0}}, 3, 'T', 0, 0};
-//Shapes
-class GameObject{
-    Board* Table;
-    Shapes* next;
-    Shapes* current;
 
-//    Shapes ShapesArray[7] =  {
-//            {(char *[]){(char[]){0, 1, 1}, (char[]){1, 1, 0}, (char[]){0, 0, 0}}, 3,
-//                                                                                     'S', 0, 0}, // S shape
-//            {(char *[]){(char[]){1, 1, 0}, (char[]){0, 1, 1}, (char[]){0, 0, 0}}, 3,
-//                                                                                     'Z', 0, 0}, // Z shape
-//            {(char *[]){(char[]){0, 1, 0}, (char[]){1, 1, 1}, (char[]){0, 0, 0}}, 3,
-//                                                                                     'T', 0, 0}, // T shape
-//            {(char *[]){(char[]){0, 0, 1}, (char[]){1, 1, 1}, (char[]){0, 0, 0}}, 3,
-//                                                                                     'L', 0, 0}, // L shape
-//            {(char *[]){(char[]){1, 0, 0}, (char[]){1, 1, 1}, (char[]){0, 0, 0}}, 3,
-//                                                                                     'J', 0, 0},                                                // J shape
-//            {(char *[]){(char[]){1, 1}, (char[]){1, 1}}, 2, 'O', 0, 0}, // O shape
-//            {(char *[]){(char[]){0, 0, 0, 0}, (char[]){1, 1, 1, 1},
-//                 (char[]){0, 0, 0, 0}, (char[]){0, 0, 0, 0}},
-//                                                                                  4, 'I', 0, 0} // I shape
-//            // you can add any shape like it's done above. Don't be naughty.
-//    };
-    struct timeval before_now, now; // time points
-    suseconds_t timer;              // time delay
-    int score;     // current score
-    int input;     // user input
-    state_t state;
+class GameObject {
 
-public:
-    GameObject();
-    ~GameObject() = default;
+    GameBoard* m_gBoard;
+    Shape* currShape;
+    Shape* nextShape;
+    stateApp state;
+    int score, bestScore;
+    /*
+    const Shape ShapesArray[7] = {
+        {(int *[]){(int[]){0, 1, 1}, (int[]){1, 1, 0}, (int[]){0, 0, 0}}, 3,
+         'S', 0, 0}, // S shape
+        {(int *[]){(int[]){1, 1, 0}, (int[]){0, 1, 1}, (int[]){0, 0, 0}}, 3,
+         'Z', 0, 0}, // Z shape
+        {(int *[]){(int[]){0, 1, 0}, (int[]){1, 1, 1}, (int[]){0, 0, 0}}, 3,
+         'T', 0, 0}, // T shape
+        {(int *[]){(int[]){0, 0, 1}, (int[]){1, 1, 1}, (int[]){0, 0, 0}}, 3,
+         'L', 0, 0}, // L shape
+        {(int *[]){(int[]){1, 0, 0}, (int[]){1, 1, 1}, (int[]){0, 0, 0}}, 3,
+         'J', 0, 0},                                                // J shape
+        {(int *[]){(int[]){1, 1}, (int[]){1, 1}}, 2, 'O', 0, 0}, // O shape
+        {(int *[]){(int[]){0, 0, 0, 0}, (int[]){1, 1, 1, 1},
+                    (int[]){0, 0, 0, 0}, (int[]){0, 0, 0, 0}},
+         4, 'I', 0, 0} // I shape
+        // you can add any shape like it's done above. Don't be naughty.
+    };
+    */
 
-
+    bool genRandomShape();
+    Shape copyShape(Shape& other);
+    void delShape();
     void writeToBoard();
-    void speedOfGame();
-    void gameControl();
-    void checkPos();
-    void getNewRandomShape();
-    void rotateShape();
-    void removeFullString();
-    void startAction();
+    bool chekPos();
+    void userAction();
+    void removeFullRows();
+    void updateScore();
     void stateMachine();
-    void getSignal();
 };
 
 
