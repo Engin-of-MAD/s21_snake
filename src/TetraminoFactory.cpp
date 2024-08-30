ccbea45b5b90e80d9454c333a29071f9ff3aa9b2
Ваше Имя <you@example.com>
2024-08-30 22:18:25 +0300

    added Tetramino Fabric

---
+++ src/TetraminoFactory.cpp
@@ -0,0 +1,161 @@
+//
+// Created by roach on 29.08.2024.
+//
+
+#include "TetraminoFactory.h"
+
+
+
+Tetromino::Tetromino(Tetromino::ShapesTypes shapeType, const Matrix2x2& shape, int cordX, int cordY)
+        : m_name(shapeType), m_width(2)
+        , m_shape(new int* [m_width])
+        , m_cordX(cordX), m_cordY(cordY){
+    for (int i = 0; i < m_width; ++i) {
+        m_shape[i] = new int[m_width];
+        std::copy(shape[i].begin(), shape[i].end(), m_shape[i]);
+    }
+}
+
+Tetromino::Tetromino(Tetromino::ShapesTypes shapeType, const Matrix3x3& shape, int cordX, int cordY)
+        : m_name(shapeType), m_width(3)
+        , m_shape(new int* [m_width])
+        , m_cordX(cordX), m_cordY(cordY){
+    for (int i = 0; i < m_width; ++i) {
+        m_shape[i] = new int[m_width];
+        std::copy(shape[i].begin(), shape[i].end(), m_shape[i]);
+    }
+}
+Tetromino::Tetromino(Tetromino::ShapesTypes shapeType, const Matrix4x4& shape, int cordX, int cordY)
+        : m_name(shapeType), m_width(4),
+          m_shape(new int* [m_width]),
+          m_cordX(cordX), m_cordY(cordY){
+    for (int i = 0; i < m_width; ++i) {
+        m_shape[i] = new int[m_width];
+        std::copy(shape[i].begin(), shape[i].end(), m_shape[i]);
+    }
+}
+
+Tetromino::Tetromino(Tetromino &other)
+        : m_cordX(other.m_cordX)
+        , m_cordY(other.m_cordY), m_name(other.m_name) {
+    delMatrix(m_shape, m_width);
+    m_width = other.m_width;
+    m_shape = createMatrix(m_shape, m_width);
+    fillMatrix(m_shape, other.m_shape, m_width);
+}
+
+Tetromino::~Tetromino() {
+    if(m_shape) {
+        for (int i = 0; i < m_width; ++i) {
+            delete[] m_shape[i];
+        }
+        delete[] m_shape;
+    }
+}
+
+
+void Tetromino::increaseCordX() {++m_cordX;}
+void Tetromino::decreaseCordX() {--m_cordX;}
+void Tetromino::increaseCordY() {++m_cordY;}
+void Tetromino::decreaseCordY() {--m_cordY;}
+void Tetromino::setCordX(int cordX) {m_cordX = cordX;}
+void Tetromino::setCordY(int cordY) {m_cordY = cordY;}
+void Tetromino::setWidth(int width) {m_width = width;}
+int Tetromino::getCordX() {return m_cordX;}
+int Tetromino::getCordY() {return m_cordY;}
+int Tetromino::getWidth() {return m_width;}
+Tetromino::ShapesTypes Tetromino::getName() { return m_name; }
+void Tetromino::printInConsole() {
+    for (int i = 0; i < m_width; ++i) {
+        for (int j = 0; j < m_width; ++j) {
+            std::cout << m_shape[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+int *Tetromino::operator[](int index) {
+    return m_shape[index];
+}
+
+Tetromino &Tetromino::operator=(const Tetromino &other) {
+    if (this == &other) return *this;
+    delMatrix(m_shape, m_width);
+    m_width = other.m_width;
+    m_cordX = other.m_cordX;
+    m_cordY = other.m_cordY;
+    m_name = other.m_name;
+    m_shape = createMatrix(m_shape, m_width);
+    fillMatrix(m_shape, other.m_shape, m_width);
+    return *this;
+}
+
+
+int **Tetromino::createMatrix(int** shape, int width) {
+    shape = new int * [width];
+    for (int i = 0; i < width; ++i) {
+        shape[i] = new int[width];
+    }
+    return shape;
+}
+
+void Tetromino::delMatrix(int **shape, int width) {
+    if (shape) {
+        for (int i = 0; i < width; ++i) {
+            delete[] shape[i];
+        }
+        delete[] shape;
+        shape = nullptr;
+    }
+}
+
+void Tetromino::fillMatrix(int **shape, int **other, int width) {
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < width; ++j) {
+            shape[i][j] = other[i][j];
+        }
+    }
+}
+
+void Tetromino::rotate() {
+    Tetromino tmpShape(*this);
+    for (int i = 0; i < tmpShape.m_width; ++i) {
+        for (int j = 0, k = tmpShape.m_width - 1; j < tmpShape.m_width; ++j, k--)
+            m_shape[i][j] = tmpShape[k][i];
+    }
+}
+
+
+Tetromino TetrominoFactory::createTetromino(Tetromino::ShapesTypes type) {
+    switch (type) {
+        case Tetromino::ShapesTypes::Z:
+            return Tetromino(type, Matrix3x3{{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}});
+        case Tetromino::ShapesTypes::S:
+            return Tetromino(type, Matrix3x3{{{0, 1, 1}, {1, 1, 0}, {0, 0, 0}}});
+        case Tetromino::ShapesTypes::T:
+            return Tetromino(type, Matrix3x3{{{0, 1, 0}, {1, 1, 1}, {0, 0, 0}}});
+        case Tetromino::ShapesTypes::L:
+            return Tetromino(type, Matrix3x3{{{0, 0, 1}, {1, 1, 1}, {0, 0, 0}}});
+        case Tetromino::ShapesTypes::J:
+            return Tetromino(type, Matrix3x3{{{1, 0, 0}, {1, 1, 1}, {0, 0, 0}}});
+        case Tetromino::ShapesTypes::O:
+            return Tetromino(type, Matrix2x2{{{1, 1}, {1, 1}}});
+        case Tetromino::ShapesTypes::I:
+            return Tetromino(type, Matrix4x4{{{0, 0, 0, 0}, {1, 1, 1, 1},
+                                              {0, 0, 0, 0}, {0, 0, 0, 0}}});
+        default:
+            throw std::invalid_argument("Unsupported tetromino type");
+    }
+}
+
+
+Tetromino TetrominoFactory::randomTetromino() {
+    const Tetromino::ShapesTypes shapesType[7] = {Tetromino::ShapesTypes::Z, Tetromino::ShapesTypes::S, Tetromino::ShapesTypes::T,
+                                                  Tetromino::ShapesTypes::L, Tetromino::ShapesTypes::J, Tetromino::ShapesTypes::O,
+                                                  Tetromino::ShapesTypes::I};
+    static std::mt19937 rnd(std::random_device{}());
+    static std::uniform_int_distribution<> dist(0, 6);
+    int randomIndex = dist(rnd);
+    return createTetromino(shapesType[randomIndex]);
+}
+
---
+++ src/TetraminoFactory.h
@@ -0,0 +1,56 @@
+//
+// Created by roach on 29.08.2024.
+//
+
+#ifndef FIGUREFACTORY_H
+#define FIGUREFACTORY_H
+
+#include <iostream>
+#include <array>
+#include <random>
+
+using Matrix4x4 = std::array<std::array<int, 4>, 4>;
+using Matrix3x3 = std::array<std::array<int, 3>, 3>;
+using Matrix2x2 = std::array<std::array<int, 2>, 2>;
+
+class Tetromino{
+
+public:
+    enum class ShapesTypes : char { Z = 'Z', S = 'S', T = 'T', L = 'L', J = 'J', O = 'O', I = 'I'};
+    Tetromino(ShapesTypes shapeType, const Matrix4x4& shape, int cordX = 0, int cordY = 0);
+    Tetromino(ShapesTypes shapeType, const Matrix3x3& shape, int cordX = 0, int cordY = 0);
+    Tetromino(ShapesTypes shapeType, const Matrix2x2& shape, int cordX = 0, int cordY = 0);
+    Tetromino(Tetromino& other);
+    ~Tetromino();
+    void printInConsole();
+    void setCordX(int cordX);
+    void setCordY(int cordY);
+    void setWidth(int width);
+    int getCordX();
+    int getCordY();
+    int getWidth();
+    ShapesTypes getName();
+    void increaseCordX();
+    void increaseCordY();
+    void decreaseCordX();
+    void decreaseCordY();
+    void rotate();
+    int* operator[](int index);
+    Tetromino& operator=(const Tetromino& other);
+
+protected:
+    int m_width, m_cordX, m_cordY;
+    int** m_shape;
+    ShapesTypes m_name;
+    int** createMatrix(int** shape, int width);
+    void delMatrix(int** shape, int width);
+    void fillMatrix(int** shape, int** other, int width);
+};
+
+class TetrominoFactory {
+public:
+    static Tetromino createTetromino(Tetromino::ShapesTypes type);
+    static Tetromino randomTetromino();
+
+};
+#endif //FIGUREFACTORY_H
