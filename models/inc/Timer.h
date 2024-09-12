8b05535da0c090eee85f736fd70ea3403fd486f8
Ваше Имя <you@example.com>
2024-09-12 21:02:46 +0300

    fix timer

--- models/inc/Timer.h
+++ models/inc/Timer.h
@@ -1,34 +1,25 @@
 //
-// Created by roach on 05.09.2024.
+// Created by roach on 12.09.2024.
 //
 
 #ifndef TIMER_H
 #define TIMER_H
 #include <chrono>
-#include <functional>
-#include <stdexcept>
 #include <iostream>
-// models/inc/Timer.h
-template<typename T, typename... Args>
+
+using Milliseconds = std::chrono::milliseconds;
+using Clock = std::chrono::steady_clock;
+
 class Timer {
-    using Model = T;
-    bool isRunning;
-    int milliseconds; // milliseconds;
-    Model* obj;
-    std::tuple<Args...> methodArguments;
-    std::function<void(Model*, Args...)> methodPtr;         // должен храниться указатель на метод класса
-    void delay();
+    Clock::time_point m_currentTime, m_lastUpdateTime;
+    Milliseconds m_timer;
 public:
     Timer();
     ~Timer() = default;
-    void begin();
-    void stop();
-    void run();
-    void setDelay(int milliseconds);
-
-    template<typename Func>
-    void setMethodInstance(Model* instance, Func func,Args... args);
-    void setMethodArguments(Args... args);
+    bool delay(Milliseconds timer);
+    void setCurrentTime(Clock::time_point timePoint);
+    void setLastUpdateTime(Clock::time_point timePoint);
 };
+
 
 #endif //TIMER_H
--- models/src/Timer.cpp
+++ models/src/Timer.cpp
@@ -1,70 +1,19 @@
 //
-// Created by roach on 05.09.2024.
+// Created by roach on 12.09.2024.
 //
+
 #include "../inc/Timer.h"
-// models/src/Timer.cpp
-template<typename T, typename... Args>
-Timer<T, Args...>::Timer() : methodPtr(nullptr), milliseconds(0)
-, isRunning(false), obj(nullptr) {}
-
-template<typename T, typename... Args>
-void Timer<T, Args...>::delay() {
-    if (!isRunning || methodPtr) return;
-    auto start = std::chrono::high_resolution_clock::now();
-    while (true) {
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        if (duration.count() >= milliseconds)
-            break;
-    }
+//models/src/Timer.cpp
+Timer::Timer() : m_lastUpdateTime(Clock::now()){}
+bool Timer::delay(Milliseconds timer) {
+    auto elapsedTime = std::chrono::duration_cast<Milliseconds>(m_currentTime - m_lastUpdateTime);
+    std::cout << "currentTime: " << m_currentTime.time_since_epoch().count() <<
+    ", lastTimeUpdate: "  << m_lastUpdateTime.time_since_epoch().count() << std::endl;
+    std::cout << "Timer: " << this << ", delay: " << m_timer.count() << ", elapsedTime: "  << elapsedTime.count() << std::endl;
+    return elapsedTime > timer;
 }
 
-template<typename T, typename... Args>
-void Timer<T, Args...>::run() {
-    delay();
-    if (!isRunning || !methodPtr || !obj) {
-        return; // Early exit if not running, method or object is null
-    }
+void Timer::setCurrentTime(Clock::time_point timePoint) { m_currentTime = timePoint; }
+void Timer::setLastUpdateTime(Clock::time_point timePoint) { m_lastUpdateTime = timePoint; }
 
-    try {
-        auto invokeArgs = std::apply([](auto&&... args) {
-            return std::tie(args...);
-        }, methodPtr);
 
-        methodPtr(obj, std::forward<decltype(invokeArgs)>(invokeArgs));
-    } catch (const std::exception& e) {
-        std::cerr << "Error in timer callback: " << e.what() << std::endl;
-    }
-}
-
-template<typename T, typename... Args>
-void Timer<T, Args...>::begin() {
-    if (!methodPtr && !obj)
-        throw std::runtime_error("Timer hasn't been configured properly");
-    isRunning = true;
-    run();
-}
-
-template<typename T, typename... Args>
-void Timer<T, Args...>::stop() {isRunning = false;}
-
-template<typename T, typename... Args>
-template<typename Func>
-void Timer<T, Args...>::setMethodInstance(Model* instance, Func func,Args... args)
-{
-    if (!instance)
-        throw std::invalid_argument("Invalid instance or method pointer");
-    methodPtr = [this, instance, func, args...]() mutable {
-        if (!func)
-            throw std::runtime_error("Null pointer instance or method");
-        return std::invoke(func, instance, std::forward<Args>(args)...);
-    };
-    obj = instance;
-}
-
-template<typename T, typename... Args>
-void Timer<T, Args...>::setMethodArguments(Args... args) {
-    methodArguments = std::make_tuple(std::forward<Args>(args)...);
-}
-template<typename T, typename... Args>
-void Timer<T, Args...>::setDelay(int milliseconds) {this->milliseconds = milliseconds;}
