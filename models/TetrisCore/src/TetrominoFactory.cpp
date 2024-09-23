a47218cc47fef864e38ab7aeeabfd0de91703045
Ваше Имя <you@example.com>
2024-09-23 09:37:00 +0300

    refactoring

--- models/TetrisCore/src/TetrominoFactory.cpp
+++ models/TetrisCore/src/TetrominoFactory.cpp
@@ -179,8 +179,8 @@ namespace s21 {
 
 
     Tetromino TetrominoFactory::randomTetromino() {
-        const Tetromino::ShapesTypes shapesType[7] = {ShapeType ::Z, ShapeType ::S, ShapeType::T,ShapeType::L
-                                                      , ShapeType::J, ShapeType::O, ShapeType::I};
+        const Tetromino::ShapesTypes shapesType[7] = {ShapeType ::Z, ShapeType ::S, ShapeType::T, ShapeType::L
+                                                     ,ShapeType::J, ShapeType::O, ShapeType::I};
         static std::mt19937 rnd(std::random_device{}());
         static std::uniform_int_distribution<> dist(0, 6);
         int randomIndex = dist(rnd);
