2850ed75cb7580874ff5c712469845b64ef4ec1d
Ваше Имя <you@example.com>
2024-09-29 17:43:46 +0300

    fix

--- gen_make.sh
+++ gen_make.sh
@@ -1,35 +1,4 @@
 mkdir build
 cd build
 cmake ..
-make all
-mkdir report
-mkdir coverage
-mkdir gcov
-./BrickGame_tests
-
-mv CMakeFiles/BrickGame_tests.dir/tests/*.gcno coverage
-mv models/SnakeCore/CMakeFiles/SnakeLib.dir/src/*.gcno coverage
-mv models/TetrisCore/CMakeFiles/TetrisLib.dir/src/*.gcno coverage
-mv models/UnionModule/CMakeFiles/UnionLib.dir/src/*.gcno coverage
-
-mv CMakeFiles/BrickGame_tests.dir/tests/*.gcda coverage
-mv models/SnakeCore/CMakeFiles/SnakeLib.dir/src/*.gcda coverage
-mv models/TetrisCore/CMakeFiles/TetrisLib.dir/src/*.gcda coverage
-mv models/UnionModule/CMakeFiles/UnionLib.dir/src/*.gcda coverage
-
-#mv CMakeFiles/BrickGame_tests.dir/tests/*.d coverage
-#mv models/SnakeCore/CMakeFiles/SnakeLib.dir/src/*.d coverage
-#mv models/TetrisCore/CMakeFiles/TetrisLib.dir/src/*.d coverage
-#mv models/UnionModule/CMakeFiles/UnionLib.dir/src/*.d coverage
-cd coverage
-
-gcov *.gcno
-
-lcov --capture --directory . --output-file coverage.info
-
-
-echo pwd
-genhtml coverage.info --output-directory ../report
-rm *.gcno *.gcda
-cmake --build .
-
+cmake -G "Unix Makefiles"
